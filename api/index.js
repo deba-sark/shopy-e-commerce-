e31a0a9a07b10b4946b286cf9a427fe36fc7c3a8
debasark <debajyotisarkar52@gmail.com>
2022-06-30 16:18:27 +0100

    commit changes

---
+++ api/index.js
@@ -0,0 +1,29 @@
+const express=require("express");
+const app=express();
+const mongoose=require("mongoose");
+const dotenv=require("dotenv");
+const userRoute=require("./routes/users")
+const authRoute=require("./routes/auth")
+const productRoute=require("./routes/product")
+const cartRoute=require("./routes/cart")
+const orderRoute=require("./routes/order")
+const stripeRoute=require("./routes/stripe")
+const cors=require("cors")
+
+dotenv.config();
+mongoose.connect(process.env.MONGO_URL).then(()=>console.log("db connection successful")).catch((err)=>{comsole.log(err)});
+app.listen(process.env.PORT||5000,()=>{
+    console.log("Backend server is runnimg!");
+});
+app.use(cors());
+app.use(express.json()); 
+app.use("/api/users",userRoute);
+app.use("/api/auth",authRoute);
+app.use("/api/products",productRoute);
+app.use("/api/carts",cartRoute);
+app.use("/api/orders",orderRoute);
+app.use("/api/checkout",stripeRoute);
+
+app.get("/api/test",()=>{
+    console.log("test is successful");
+});
